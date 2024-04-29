cead22e5481c523c8d97622b61765fcdc114ea39
sudolmunib <162574378+sudolmunib@users.noreply.github.com>
2024-04-29 13:47:18 +0700

    Add files via upload

---
+++ guided1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Mendefinisikan struktur buku
+struct buku {
+    string judulBuku;
+    string pengarang;
+    string penerbit;
+    int tebalHalaman;
+    int hargaBuku;
+};
+
+int main() {
+    // Mendeklarasikan variabel favorit dengan tipe buku
+    buku favorit;
+
+    // Mengisi data ke dalam variabel favorit
+    favorit.judulBuku = "TRIK AHLI NERAKA";
+    favorit.pengarang = "Abdul munib";
+    favorit.penerbit = "Gagas Media";
+    favorit.tebalHalaman = 20;
+    favorit.hargaBuku = 50000;
+
+    // Menampilkan informasi buku favorit
+    cout << "\tBuku Favorit Saya" << endl;
+    cout << "\tJudul Buku : " << favorit.judulBuku << endl;
+    cout << "\tPengarang : " << favorit.pengarang << endl;
+    cout << "\tPenerbit : " << favorit.penerbit << endl;
+    cout << "\tTebal Halaman: " << favorit.tebalHalaman << " halaman" << endl;
+    cout << "\tHarga Buku : Rp " << favorit.hargaBuku << endl;
+    return 0;
+}
